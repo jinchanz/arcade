0533f3fa8f3c01f848d9405203467b2439d0da6b
jinchan <892331011@qq.com>
2024-04-22 03:34:28 +0800

    fix: sign in dismiss

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -32,7 +32,7 @@ export default function () {
                       <User user={user} />
                     </>
                   ) : (
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <div className="lg:flex lg:flex-1 lg:justify-end">
                       <a href="/sign-in" className="text-sm font-semibold leading-6 text-gray-900">
                         Log in <span aria-hidden="true">&rarr;</span>
                       </a>
